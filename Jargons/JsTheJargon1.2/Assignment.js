586b3d8e74990a0e0a9ff9e7914c61396cd41534
SHAHIMA FIROZ KHAN <99420269+Shahimafiroz@users.noreply.github.com>
2024-02-17 04:50:28 +0530

    JS

---
+++ Jargons/JsTheJargon1.2/Assignment.js
@@ -0,0 +1,31 @@
+// Assignment Problem 1
+
+// for (i = 0; i <= 30; i++) {
+//   console.log(i);
+// }
+
+//Assignment Problem 2
+
+// setTimeout(a, 1000);
+// console.time("label");
+
+// function a() {
+//   console.timeEnd("label");
+// }
+
+// Assignment  3
+
+function ShowTime() {
+  let time = new Date();
+  let hour = time.getHours();
+  let min = time.getMinutes();
+  let sec = time.getSeconds();
+
+  let currentTime = hour + ":" + min + ":" + sec;
+
+  console.log(currentTime);
+}
+
+setInterval(ShowTime, 1 * 1000);
+
+ShowTime();
---
+++ Jargons/JsTheJargon1.2/Callback.js
@@ -0,0 +1,6 @@
+function greet() {
+  console.log("Hi shahima !!");
+}
+
+setTimeout(greet, 4 * 1000);
+setInterval(greet, 1 * 1000);
